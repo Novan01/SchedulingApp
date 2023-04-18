184cc9ebd3a01540c3a7fdb5aaf52dc2fdbe7c0f
Novan01 <patrick@casaanderson.net>
2023-04-18 15:13:03 -0500

    Event.java

---
+++ Event.java
@@ -0,0 +1,58 @@
+import java.sql.Date;
+import java.sql.*;
+import java.time.*;
+
+public class Event {
+    int id, priority;
+    Date time;
+    String name, description;
+
+    public Event(int i, int prio, Date t, String n, String d) {
+        int id = i;
+        int priority = prio;
+        Date time = t;
+        String name = n;
+        String description = d;
+
+    }
+
+    public int getID() {
+        return this.id;
+    }
+
+    public void setID(int ID) {
+        this.id = ID;
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
+
+    public void setPriority(int prio) {
+        this.priority = prio;
+    }
+
+    public Date getDate() {
+        return this.time;
+    }
+
+    public void setDate(Date t) {
+        this.time = t;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String desc) {
+        this.description = desc;
+    }
+}
